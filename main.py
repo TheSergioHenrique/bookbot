2652a7f550a15f469ef3e8ef10b954774cfca935
TheSergioHenrique <sergio5203@gmail.com>
2024-06-20 14:01:54 -0300

    Finished text reading function. added 'print' output.

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 def main():
     path = "books/frankenstein.txt"
     text = book_text(path)
+    number_of_words = num_words_in_book(text)
+    print(f"THE NUMBER OF WORDS IN THIS DOCUMENT IS: {number_of_words}.")
+
+
     #função para pegar o texto do livro
-    def book_text(path):
-        #WIP
+def book_text(path):
+    with open(path) as text:
+        return text.read()
+
+
     #função para conseguir o número de palavras no texto
-    def num_words_in_book(path):
-        words = text.split()
-        return f.read()
+def num_words_in_book(path):
+    words = path.split()
+    return len(words)
+
+
+
 main()
