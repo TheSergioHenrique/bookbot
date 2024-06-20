ca89de71a2b30ba8c0394d342a74eeda625c9437
TheSergioHenrique <sergio5203@gmail.com>
2024-06-20 14:32:23 -0300

    added letter count function. non-alphabetic characters included.

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     path = "books/frankenstein.txt"
     text = book_text(path)
     number_of_words = num_words_in_book(text)
+    letter_count = letter_counting(text)
     print(f"THE NUMBER OF WORDS IN THIS DOCUMENT IS: {number_of_words}.")
+    print (letter_count)
 
 
     #função para pegar o texto do livro
@@ -15,6 +17,18 @@ def book_text(path):
 def num_words_in_book(path):
     words = path.split()
     return len(words)
+
+    #função para converter o texto para lowercase, e contar quantas letras de cada aparecem no livro.
+def letter_counting(text):
+    lowered_string = text.lower()
+    dict = {}
+    for value in lowered_string:
+        if value in dict:
+            dict[value] += 1
+        else:
+            dict[value] = 1
+    return dict
+
 
 
 
