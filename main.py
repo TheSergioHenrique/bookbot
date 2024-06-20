5fbecb6cf81dc26093555bdf4609757480a8516b
TheSergioHenrique <sergio5203@gmail.com>
2024-06-19 22:56:35 -0300

    WIP Book_text function.

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    path = "books/frankenstein.txt"
+    text = book_text(path)
+    #função para pegar o texto do livro
+    def book_text(path):
+        #WIP
+    #função para conseguir o número de palavras no texto
+    def num_words_in_book(path):
+        words = text.split()
+        return f.read()
+main()
